5715f7578dcfc39c52cc42031d51ec9ed879f83c
Daisuke Kobayashi <smallcopse@users.noreply.github.com>
2022-06-17 20:50:48 +0900

    flexible number picker

--- killer-sudoku-practice/src/App.tsx
+++ killer-sudoku-practice/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <SudokuView key="sudokuView" fontFamily='Roboto Mono'/>
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -20,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
